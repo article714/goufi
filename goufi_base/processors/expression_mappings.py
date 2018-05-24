28b7267a7d7d2a015b66fa0fe80c76c8ca20d200
xtof <christophe@openflexo.org>
2018-05-24 10:38:02 +0200

    [FIX] typo

--- goufi_base/processors/expression_mappings.py
+++ goufi_base/processors/expression_mappings.py
@@ -142,7 +142,7 @@ class ExpressionProcessorMixin(object):
 
     def map_values(self, row):
         result = copy(row)
-        keys = copy(row.keys)
+        keys = copy(row.keys())
         for f in keys:
             if f in self.col2fields:
                 # replace non json-compatible values
